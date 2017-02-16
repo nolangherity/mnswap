b334ff13d606c59296721903a10d7ff0db1aaad2
Nolan Gherity <nolangherity@ng.local>
2017-02-16 14:45:48 -0600

    added signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root 'pages#home'
   get  '/help',    to: 'pages#help'
   get  '/about',   to: 'pages#about'
   get  '/contact', to: 'pages#contact'
+  get  '/signup',  to: 'users#new'
 end
