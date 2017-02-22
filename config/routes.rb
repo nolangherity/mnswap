17603175738ed3d2f66e85b23657169e9619c184
Nolan Gherity <ngherity@gmail.com>
2017-02-22 16:28:02 -0600

    finished login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 
-  root 'pages#home'
-  get  '/help',    to: 'pages#help'
-  get  '/about',   to: 'pages#about'
-  get  '/contact', to: 'pages#contact'
-  get  '/signup',  to: 'users#new'
+  root   'pages#home'
+  get    '/help',    to: 'pages#help'
+  get    '/about',   to: 'pages#about'
+  get    '/contact', to: 'pages#contact'
+  get    '/signup',  to: 'users#new'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :users
 end
