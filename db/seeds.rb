5548af4a9b7748bb1025ecf102b6661e6310a615
nolangherity <ngherity@gmail.com>
2017-02-23 22:49:07 -0600

    update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,1 @@
-User.create!(name:  "Admin",
-             email: "admin@mnswap.com",
-             password:              "boge703205",
-             password_confirmation: "boge703205",
-             admin:     true,
-             activated: true,
-             activated_at: Time.zone.now)
+
